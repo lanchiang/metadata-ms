d3dade506bcd91469e7a7f1fa80d0e87573d4377
Lan Jiang <jianglan1350@gmail.com>
2017-02-12 13:54:03 +0100

    add Normalize interface

---
+++ mdms/mdms-java/src/main/java/de/hpi/isg/mdms/java/feature/NormalizationHandler.java
@@ -0,0 +1,14 @@
+package de.hpi.isg.mdms.java.feature;
+
+/**
+ * An interface to supply the normalize function for the features.
+ * @author Lan Jiang
+ * @since 11/02/2017
+ */
+public interface NormalizationHandler {
+
+    /**
+     * Normalize the feature.
+     */
+    void normalize();
+}
